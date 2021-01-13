58d604771f0c2c4e81796873e7cc001539f4e75e
ardzz <ardzz@indoploit.or.id>
2021-01-14 06:19:12 +0700

    Add PHPDoc and remove some unused variable

--- src/User.php
+++ src/User.php
@@ -3,28 +3,51 @@
 
 namespace OpenWAClient;
 
-use OpenWAClient\Factory;
 use OpenWAClient\FileManager\Media;
 use OpenWAClient\Handler\Output;
 use OpenWAClient\Handler\Response;
-use OpenWAClient\Send\SendFactory;
 
+/**
+ * Class User
+ *
+ * This class have a responsibility to handle the User tasks
+ *
+ * @package OpenWAClient
+ */
 class User extends OpenWA
 {
+    /**
+     *
+     * Get account info (self fetching)
+     *
+     * @return Output
+     */
     function getMe(): Output
     {
         $request = $this->request("getMe");
-        $response = $request->getBody()->getContents();
         return new Output(new Response($request));
     }
 
+    /**
+     *
+     * Get session info from Server RESt API
+     *
+     * @return Output
+     */
     function getSessionInfo(): Output
     {
         $request  = $this->request("getSessionInfo");
-        $response = $request->getBody()->getContents();
         return new Output(new Response($request));
     }
 
+    /**
+     *
+     * Set status account (e.g, Available, Busy, On meeting, Sleep, ...)
+     *
+     * @param     String     $status
+     *
+     * @return Output
+     */
     function setStatus(String $status): Output
     {
         $request = $this->request("setMyStatus", [
@@ -32,10 +55,17 @@ class User extends OpenWA
                 "newStatus" => $status
             ]
         ]);
-        $response =  $request->getBody()->getContents();
         return new Output(new Response($request));
     }
 
+    /**
+     *
+     * Set name account
+     *
+     * @param     String     $name
+     *
+     * @return Output
+     */
     function setName(String $name): Output
     {
         $request = $this->request("setMyName", [
@@ -43,24 +73,24 @@ class User extends OpenWA
                 "newName" => $name
             ]
         ]);
-        $response =  $request->getBody()->getContents();
         return new Output(new Response($request));
     }
 
+    /**
+     *
+     * Set profile picture from image file
+     *
+     * @param     String     $file_name
+     *
+     * @return Output
+     */
     function setProfilePicture(String $file_name): Output
     {
-        $data = Media::format($file_name);
-        if ($data){
-            $request = $this->request("setProfilePic", [
-                "args" => [
-                    "data" => $data
-                ]
-            ]);
-            $response =  $request->getBody()->getContents();
-            return new Output(new Response($request));
-        }
-        return $this->handleOutput(json_encode([
-            "success" => false
-        ]));
+        $request = $this->request("setProfilePic", [
+            "args" => [
+                "data" => Media::format($file_name)
+        ]
+        ]);
+        return new Output(new Response($request));
     }
 }
